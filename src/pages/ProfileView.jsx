43042d3d5a65a251a073b7e7bd7e8bfeca97b161
rahamath45 <rahmathsulaiman45@gmail.com>
2025-12-17 12:42:35 +0530

    service/api

--- src/pages/ProfileView.jsx
+++ src/pages/ProfileView.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import api from "../api";
+import api from "../services/api";
+
 
 const ProfileView = () => {
   const [profile, setProfile] = useState(null);
