ce8b16b281f40f72327f2def2cf61a4aa11dfc20
rahamath45 <rahmathsulaiman45@gmail.com>
2025-12-17 11:37:03 +0530

    api.js changed

--- src/services/api.js
+++ src/services/api.js
@@ -5,10 +5,13 @@ const api = axios.create({ baseURL: "https://intership-et7g.onrender.com/api" })
 
 
 api.interceptors.request.use(config => {
-const token = localStorage.getItem("token");
-if (token) config.headers.authorization = token;
-return config;
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
+
 
 
 export default api;
